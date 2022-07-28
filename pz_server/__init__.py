0fe24edce27ad992025cd5f537c16f4e9d00e5b3
gschwend <julia@linea.org.br>
2022-07-28 17:21:43 -0300

    Separate file for plot functions

--- pz_server/__init__.py
+++ pz_server/__init__.py
@@ -1,1 +1,2 @@
 from .core import PzServer
+from .plots import *
---
+++ pz_server/plots.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#import seaborn as sns
+
+
+def specz_plots(data=None, metadata=None, savefig="False"):
+    """ 
+    Basic plots to characterize a Spec-z catalog. 
+
+    Args: 
+        data: 
+        metadata:
+        savefig: 
+
+    Returns: 
+
+    """
+    raise NotImplementedError
