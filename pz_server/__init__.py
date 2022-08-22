d052c9e7ce5cdc4358f7deb495396ff9b28540d7
Julia Gschwend <julia@linea.org.br>
2022-08-22 15:52:37 -0300

    Feat/tutorial (#6)
    * Start tutorial notebook (moved from pz-server main repository)
    
    * Update tutorial notebook text cells
    
    * Update .gitignore with VS Code hidden files
    
    * Start coding the base class PzServer
    
    * Update tutorial notebook text
    
    * Start writing lib documentation
    
    * Add setup.py
    
    * Add empty test file
    
    * setup.py copied and adapted from dblinea
    
    * Add core functions docstrings
    
    * Add placeholder for API Token to PzServer class
    
    * Separate file for plot functions
    
    * Add json files with data for tests
    
    * Add contents to product_metadata.json
    
    * update documentation files
    
    * add contents to test data file
    
    * update logo linea url

---
+++ pz_server/__init__.py
@@ -0,0 +1,2 @@
+from .core import PzServer
+from .plots import *
---
+++ pz_server/core.py
@@ -0,0 +1,102 @@
+import requests
+
+import numpy as np
+#import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+#import seaborn as sns
+
+
+class PzServer():
+
+    def __init__(self, token):
+        # token
+
+        pass
+
+    def list_product_types(self):
+        """Fetches the list of valid product types. 
+
+        Connects to the Photo-z Server's admnistrative 
+        database and fetches the list of valid product 
+        types and their respective short description.
+
+        Returns:
+            A dict mapping product type names to the 
+            corresponding description. 
+        """
+
+        raise NotImplementedError
+
+    def list_users(self):
+        """Fetches the list of registered users. 
+
+        Connects to the Photo-z Server's admnistrative 
+        database and fetches the list of registered 
+        users (github username). 
+
+        Returns:
+            A list of github usernames.             
+        """
+
+        raise NotImplementedError
+
+    def list_releases(self):
+        """Fetches the list of valid data releases. 
+
+        Connects to the Photo-z Server's admnistrative
+        database and fetches the list of valid LSST
+        data releases corresponding to the data products
+        available. The result list is expected to
+        increase over the years of survey operations.
+
+        Returns:
+            A list data release tags.  
+       """
+
+        raise NotImplementedError
+
+    def list_products(self, filters=None):
+        """Fetches the list of data products available. 
+
+        Connects to the Photo-z Server's database and 
+        fetches the filtered list of data products 
+        available. The (optional) filters are provided 
+        as dictionary by the user as argument. Default 
+        is no filter.  
+
+        Returns:
+            A dict mapping data products to the corresponding 
+            short description informed by the owners.             
+        """
+        raise NotImplementedError
+
+    def get_product_metadata(self, product_id=None):
+        """Fetches the product metadata. 
+
+        Connects to the Photo-z Server's database and 
+        fetches the metadata available for a given 
+        data product located by product_id (provided 
+        as argumetn by the user). 
+
+        Returns:
+            A dict with data product metadata informed
+            py the product owner. 
+        """
+        raise NotImplementedError
+
+    def get_product(self, product_id=None):
+        """Fetches the data to local. 
+
+        Connects to the Photo-z Server's database and 
+        fetches the data stored as registered data 
+        product. The result depend on product type. 
+        If tabular data, returns Astropy Table. If 
+        multiple files, download a .tar file to local
+        environment and returns string with file name. 
+
+        Returns:
+            Astropy Table with tabular data or 
+            .tar file (in case of multiple files). 
+        """
+        raise NotImplementedError
