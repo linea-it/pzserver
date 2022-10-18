cfba9126718b8f1481d06ed2623ab1e6b9fd97b4
gschwend <julia@linea.org.br>
2022-10-18 19:47:47 -0300

    function get_product done, tutorial notebook done up to spec-z catalaog

--- pz_server/core.py
+++ pz_server/core.py
@@ -3,11 +3,11 @@ from curses.ascii import controlnames
 import requests
 import numpy as np
 import pandas as pd
-pd.options.display.max_colwidth = None
-from IPython.display import display 
+from IPython.display import display
 import matplotlib.pyplot as plt
 from .api import PzServerApi
 import tables_io
+pd.options.display.max_colwidth = None
 
 
 class PzServer():
@@ -28,16 +28,15 @@ class PzServer():
         If called from a Jupyter notebbok, displays a 
         pandas.io.formats.style.Styler object mapping the 
         product type names to the corresponding descriptions.
-        
+
         """
         results_dict = self.api.get_all("product-types")
-        dataframe = pd.DataFrame(results_dict, 
-                    columns=["display_name", "description"])          
-        dataframe.rename(columns={"display_name": "product_type"}, inplace=True)
-        
-        display(dataframe.style.hide(axis="index"))
+        dataframe = pd.DataFrame(results_dict,
+                                 columns=["display_name", "description"])
+        dataframe.rename(
+            columns={"display_name": "product_type"}, inplace=True)
 
-        
+        display(dataframe.style.hide(axis="index"))
 
     def list_users(self):
         """Fetches the list of registered users. 
@@ -53,13 +52,12 @@ class PzServer():
         """
 
         results_dict = self.api.get_all("users")
-        dataframe = pd.DataFrame(results_dict, 
-                    columns=[ "username", "last_name"])
-        dataframe.rename(columns={"last_name": "user"}, 
-                    inplace=True)
-            
-        display(dataframe.style.hide(axis="index"))
+        dataframe = pd.DataFrame(results_dict,
+                                 columns=["username", "last_name"])
+        dataframe.rename(columns={"last_name": "user"},
+                         inplace=True)
 
+        display(dataframe.style.hide(axis="index"))
 
     def list_releases(self):
         """Fetches the list of valid data releases. 
@@ -76,13 +74,12 @@ class PzServer():
         """
 
         results_dict = self.api.get_all("releases")
-        dataframe = pd.DataFrame(results_dict, 
-                    columns=["display_name", "description"])
-        dataframe.rename(columns={"display_name": "release"}, 
-                    inplace=True)
-            
-        display(dataframe.style.hide(axis="index"))
+        dataframe = pd.DataFrame(results_dict,
+                                 columns=["display_name", "description"])
+        dataframe.rename(columns={"display_name": "release"},
+                         inplace=True)
 
+        display(dataframe.style.hide(axis="index"))
 
     def list_products(self, filters=None):
         """Fetches the list of data products available. 
@@ -97,29 +94,28 @@ class PzServer():
         pandas.io.formats.style.Styler object with the 
         list of all products available with the metadata
         informed by the owners.
-          
+
         Args:
             filters (dict): dictionary with strings (or a 
                             list of strings) patterns 
                             to filter the results. 
-                               
+
         """
         results_dict = self.api.get_products(filters)
-        dataframe = pd.DataFrame(results_dict, 
-                    columns=["id", "display_name", 
-                    "product_type_name", "survey", "release_name",  
-                    "uploaded_by", "official_product",  #"pz_code", 
-                    "description", "created_at"])
+        dataframe = pd.DataFrame(results_dict,
+                                 columns=["id", "display_name",
+                                          "product_type_name", "survey", "release_name",
+                                          "uploaded_by", "official_product",  # "pz_code",
+                                          "description", "created_at"])
 
-
-        dataframe.rename(columns={"display_name": "product_name", 
+        dataframe.rename(columns={"display_name": "product_name",
                                   "release_name": "release",
-                                  "product_type_name": "product_type"},                  
-                        inplace=True)
-        
+                                  "product_type_name": "product_type"},
+                         inplace=True)
+
         display(dataframe.style.hide(axis="index"))
 
-    #--------------------------------------------------# 
+    #--------------------------------------------------#
 
     def get_product_metadata(self, product_id=None, display_table=True):
         """Fetches the product metadata. 
@@ -129,21 +125,27 @@ class PzServer():
         data product located by product_id (provided 
         as argumetn by the user). 
 
+        Args:
+            product_id (int): data product unique identifier 
+
+            display_table (boolean): show as styled dataframe,
+                                     default=True
+
         Returns:
             A Pandas DataFrame with data product metadata 
             informed by the product owner. 
         """
 
         results_dict = self.api.get("products", product_id)
-    
+
         if display_table:
-            columns=["id", "display_name", 
-                        "product_type_name", "survey", "release_name",  
-                        "uploaded_by", "official_product",  #"pz_code", 
-                        "description", "created_at"]                   
+            columns = ["id", "display_name",
+                       "product_type_name", "survey", "release_name",
+                       "uploaded_by", "official_product",  # "pz_code",
+                       "description", "created_at"]
             transposed_list = []
-            for k,v in results_dict.items():
-                if k in columns: 
+            for k, v in results_dict.items():
+                if k in columns:
                     if k == "release_name":
                         k = "release"
                     if k == "product_type_name":
@@ -151,13 +153,12 @@ class PzServer():
                     if k == "display_name":
                         k = "product_name"
                     transposed_list.append({"key": k, "value": v})
-            dataframe = pd.DataFrame(transposed_list)        
+            dataframe = pd.DataFrame(transposed_list)
             display(dataframe.style.hide(axis="index"))
 
         return results_dict
 
-        
-    def get_product(self, product_id=None, save_file=False, tabletype=tables_io.types.PD_DATAFRAME):
+    def get_product(self, product_id=None, save_file_as=False):
         """Fetches the data to local. 
 
         Connects to the Photo-z Server's database and 
@@ -171,18 +172,29 @@ class PzServer():
         to local environment and returns a string with 
         the file name. 
 
+        Args:
+            product_id (int): data product unique identifier 
+
+            save_file_as (str/boolean): file name to save 
+                                    the data, default=False
+
         Returns:
             Tabular object with data (default is Pandas 
             Dataframe) or .tar file (in case of multiple files). 
         """
-        
         results_dict = self.api.get_content(product_id)
-        #transposed_list = []
-        #for k,v in results_dict.items():
-            #transposed_list.append({"key": k, "value": v})
-        #dataframe = pd.DataFrame(transposed_list) 
+        dataframe = pd.DataFrame(results_dict)
 
-        dataframe = pd.DataFrame(results_dict) 
-        return dataframe 
+        if save_file_as:
+            tables_io_formats = ['fits', 'hf5', 'hdf5', 'fit', 'h5', 'pq']
+            fmt = save_file_as.split('.')[-1]
+            if fmt == "csv":
+                dataframe.to_csv(save_file_as)
+            elif fmt in tables_io_formats:
+                tables_io.write(dataframe, save_file_as)
+            else:
+                raise ValueError("File format is not supported. \n"     
+                                 "Please provide a file name with one of the suffixes:\n" 
+                                 "['fits', 'hf5', 'hdf5', 'fit', 'h5', 'pq'] ")
 
-        #return results_dict 
+        return dataframe
