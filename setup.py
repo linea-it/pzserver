9f40dd6682db8b0b742de1905781e7dc65e834a2
Cristiano Singulani <crisingulani@gmail.com>
2022-11-07 11:10:45 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 setup(
     name="pz-server-lib",
     packages=find_packages(include=["pz_server"]),
-    version="0.1.0.dev2",
+    version="0.1.5.dev",
     description=(
         "Python library to access the Photo-z Server database"
         " hosted by the Brazilian LSST IDAC at LIneA. "
